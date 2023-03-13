f57fa7d3faa5e559976e53fb13b3d59daf77b458
James Beard <jbeardok@outlook.com>
2023-03-13 02:58:54 -0500

    Add styles.css to display grid cells

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const body = document.querySelector('body');
 const  grid = document.createElement('div');
-grid.id = 'grid';
+grid.classList = 'grid';
 body.appendChild(grid);
+
 for (let i = 0; i < 256; i++) {
     const square = document.createElement('div');
-    square.id = `square${i}`;
+    square.classList = `square`;
     grid.appendChild(square);
 }
